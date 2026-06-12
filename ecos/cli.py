7c6756fd875efafbf11fae7a8e210bb9126267a5
Wangzi666 <ilchenkovs81@gmail.com>
2026-06-13 02:58:20 +0300

    SPRINT X - description

--- ecos/cli.py
+++ ecos/cli.py
@@ -1,5 +1,18 @@
 ﻿import sys
-from ecos.main import run_command
+from ecos.compiler.sprint_compiler import SprintCompiler
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ecos build sprint <version>")
+        return
+
+    command = sys.argv[1]
+    entity = sys.argv[2]      # sprint
+    target = sys.argv[3]      # 2.1 / 3 / 10
+
+    if command == "build":
+        compiler = SprintCompiler()
+        compiler.run(entity, target)
 
 if __name__ == "__main__":
-    run_command(sys.argv[1], sys.argv[2], sys.argv[3] if len(sys.argv)>3 else None)
+    main()
